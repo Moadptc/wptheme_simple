5aaad127fa410d525603549f24394c7953a7f5b9
moadptc <mouadptc@gmail.com>
2018-06-11 23:28:35 +0000

    add pages about and services and menu

--- functions.php
+++ functions.php
@@ -4,6 +4,12 @@
 function simple_theme_setup()
 {
 	add_theme_support('post-thumbnails');
+
+	//Menus
+	register_nav_menus(array(
+		'primary' => __('Primary Menu')
+	));
+
 }
 
 add_action('after_setup_theme' , 'simple_theme_setup');
