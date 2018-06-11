3893928acd65384e2a5b25eca589d4cb81da70df
moadptc <mouadptc@gmail.com>
2018-06-11 04:23:09 +0000

    basic index page

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+	<div class="container">
+		<p>
+			Copyright &copy; <?php echo date('Y') ?>
+			all rights reserved by <?php bloginfo('name') ?>
+		</p>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
